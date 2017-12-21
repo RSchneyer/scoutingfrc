1383aea1dc9825bf333b0dc5688591e0c2df0810
Reid <reschneyer89@gmail.com>
2017-12-21 16:14:52 -0600

    Signing in and out is handled with AngularFire
    And it works!

--- public/app.js
+++ public/app.js
@@ -1,27 +1,28 @@
-var app = angular.module('scoutingfrc', ['ngMaterial']);
+var app = angular.module('scoutingfrc', ['ngMaterial', 'firebase']);
 
-app.controller('authControl', ['$scope', '$rootScope', '$log', '$http', function($scope, $log, $rootScope, $http){
+app.controller('authControl', ['$scope', '$rootScope', '$http', '$firebaseAuth', function($scope, $rootScope, $http, $firebaseAuth){
 	$scope.authStatus = false;
 
 	var provider = new firebase.auth.GoogleAuthProvider();
-	var db = firebase.firestore();
+	var auth = $firebaseAuth();
 
-	$scope.signIn = function () {
-		firebase.auth().signInWithPopup(provider).then(function(result){
-			$rootScope.user = result.user;
-			// $scope.authStatus = true;
-		});
-		$scope.authStatus = true;
-	}
-
-	$scope.signOut = function (){
-		firebase.auth().signOut().then(function(){
-			$scope.authStatus = false;
+	$scope.signIn = function(){
+		auth.$signInWithPopup('google').then(function(result){
+			console.dir(result);
+			$scope.authStatus = true;
 		}).catch(function(error){
-			console.log("An error occured: ", error);
+			console.error("Authentication failed: ", error);
 		});
 	};
 
+	$scope.signOut = function(){
+		auth.$signOut().then(function(){
+			console.log('Signed Out!');
+			$scope.authStatus = false;
+		});
+	};
+
+	// TODO: Move to appropriate Directive
 	$scope.getTeamData = function(){
 		var team = 'frc' + $scope.frcTeam;
 		var info = $http.get('https://www.thebluealliance.com/api/v3/team/'+team+'/simple?X-TBA-Auth-Key=sLym63lk04kq6G9IwWsvzNxrSl7DYNoyH09RRHfj7trmskoWE8bTrVTjQ8nByZ8Z')
@@ -31,3 +32,9 @@ app.controller('authControl', ['$scope', '$rootScope', '$log', '$http', function
 
 	};
 }]);
+
+app.directive('teamInputCard', function(){
+	return {
+		templateUrl: 'teamInputCard.html',
+	}
+});
