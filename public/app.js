87dcb29ec28dc15c7ae2c46f0cfee4e643809ae2
Reid <reschneyer89@gmail.com>
2017-12-20 21:04:06 -0600

    Google Auth, TBA API Call

--- public/app.js
+++ public/app.js
@@ -1,8 +1,33 @@
-function firebaseAuth(){
+var app = angular.module('scoutingfrc', ['ngMaterial']);
+
+app.controller('authControl', ['$scope', '$rootScope', '$log', '$http', function($scope, $log, $rootScope, $http){
+	$scope.authStatus = false;
+
 	var provider = new firebase.auth.GoogleAuthProvider();
-	firbase.auth().signInWithPopup(provider).then(function(result){
-		var user = result.user;
-		alert('User: '+ user);
-	});
-	
-};
+	var db = firebase.firestore();
+
+	$scope.signIn = function () {
+		firebase.auth().signInWithPopup(provider).then(function(result){
+			$rootScope.user = result.user;
+			// $scope.authStatus = true;
+		});
+		$scope.authStatus = true;
+	}
+
+	$scope.signOut = function (){
+		firebase.auth().signOut().then(function(){
+			$scope.authStatus = false;
+		}).catch(function(error){
+			console.log("An error occured: ", error);
+		});
+	};
+
+	$scope.getTeamData = function(){
+		var team = 'frc' + $scope.frcTeam;
+		var info = $http.get('https://www.thebluealliance.com/api/v3/team/'+team+'/simple?X-TBA-Auth-Key=sLym63lk04kq6G9IwWsvzNxrSl7DYNoyH09RRHfj7trmskoWE8bTrVTjQ8nByZ8Z')
+		.then(function(response){
+			$scope.teamData = response.data;
+		});
+
+	};
+}]);
