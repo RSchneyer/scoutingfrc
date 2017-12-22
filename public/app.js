bc33073eb9ccbbbfb6a24870af61e0e5a2da3080
Reid <reschneyer89@gmail.com>
2017-12-21 18:52:50 -0600

    Added function that adds new user to DB after auth

--- public/app.js
+++ public/app.js
@@ -2,18 +2,37 @@ var app = angular.module('scoutingfrc', ['ngMaterial', 'firebase']);
 
 app.controller('authControl', ['$scope', '$rootScope', '$http', '$firebaseAuth', function($scope, $rootScope, $http, $firebaseAuth){
 	$scope.authStatus = false;
-
-	var provider = new firebase.auth.GoogleAuthProvider();
 	var auth = $firebaseAuth();
+
+	var db = firebase.firestore();
+	var usersDB = db.collection('users');
 
 	$scope.signIn = function(){
 		auth.$signInWithPopup('google').then(function(result){
+			var authResult = result;
+			usersDB.doc(authResult.user.uid).get().then(function(result){
+				if (result.exists) {
+					console.log("You Exist. Congrats.")
+				} else {
+					usersDB.doc(authResult.user.uid).set({
+						userDisplayName: authResult.user.displayName
+					})
+					.then(function(){
+						console.log('User created!')
+					})
+					.catch(function(error){
+						console.log('An error occurred: ', error);
+					})
+				};
+			})	
+
+
 			console.dir(result);
 			$scope.authStatus = true;
 		}).catch(function(error){
 			console.error("Authentication failed: ", error);
 		});
-	};
+	};	
 
 	$scope.signOut = function(){
 		auth.$signOut().then(function(){
