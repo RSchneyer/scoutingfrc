c5432c8a49af9597a8b91902d8e63740b9e72066
ryradack <rjradack12@gmail.com>
2018-01-03 13:51:07 -0600

    Moved controllers out of app.js

--- public/app.js
+++ public/app.js
@@ -37,51 +37,6 @@ app.run(function($rootScope){
 });
 
 
-
-
-
-
-
-
-
-app.controller('navControl', ['$scope', '$mdSidenav', function($scope, $mdSidenav){
-	$scope.togglesideNav = function(){
-		$mdSidenav('left').toggle();
-		console.log('SideNav toggled');
-	};	
-}])
-
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// 
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-app.controller('outputControl', ['$scope', '$http', function($scope, $http){
-	var db = firebase.firestore();
-	$scope.options = [];
-	$scope.downloadEvent = function(){
-
-	};
-	//initialiaze the competitions to pick in the dropdown box
-	var init = function(){
-		var rootRef = db.collection("events/");
-		var evets = rootRef.get()
-		.then(snapshot => {
-			snapshot.forEach(doc => {
-				docData = doc.data();
-				var element = {};
-				element.name = docData.short_name;
-				element.id = doc.id;
-				$scope.options.push(element);
-				$scope.$apply();
-			})
-		})
-	}
-	init();
-}]);
-
-
-
-
 //Directives ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 app.directive('teamInputCard', function(){
 	return {
