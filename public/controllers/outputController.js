d9253a4507ae25ed9dfb68c478f932a6a634d35d
ryradack <rjradack12@gmail.com>
2018-01-04 16:25:20 -0600

    CSV downloads now downloads data by event

--- public/controllers/outputController.js
+++ public/controllers/outputController.js
@@ -1,10 +1,38 @@
 app.controller('outputControl', ['$scope', '$http', function($scope, $http){
 	var db = firebase.firestore();
 	$scope.options = [];
-	$scope.csvArray = [{a:1, c:"c", d:"d"}, {b:"1", c:"3", a:"5"}, {a:"h", b:"8", c:"d"}];
-	
+	$scope.csvArray = [];
+	$scope.csvHeader = {a:"Team #", b:"Match", c:"Auto Shot", d:"Tele Scores", e:"Tele Flag"};
+
+	$scope.getTeamCSV = function(element){
+		element = element.substr(3);
+		var rootRef = db.collection('/teams/'+element+'/events/'+$scope.exportCompetition.id+'/matches');
+		var matches = rootRef.get()
+		.then(snapshot => {
+			snapshot.forEach(doc => {
+				var jsonData = doc.data();
+				console.log(jsonData);
+				for(var p in jsonData){
+					var row = {
+						teamNum:element,
+						matchNum:doc.id,
+						autoShot:jsonData[p].autoShot,
+						teleScores:jsonData[p].teleScores,
+						teleFlag:jsonData[p].teleFlag
+					};
+					$scope.csvArray.push(row);
+				};
+			});
+		});
+	};
 	$scope.csvCompChange = function(){
-		
+		var rootRef = db.doc("events/"+$scope.exportCompetition.id);
+		var teams = rootRef.get()
+		.then(doc => {
+			data = doc.data();
+			console.log(data.teams);
+			data.teams.forEach($scope.getTeamCSV);
+		});
 	};
 	//initialiaze the competitions to pick in the dropdown box
 	$scope.init = function(){
