45194eefc36aebdc88a107a3b82623f0bd70047c
phekelsey <phekelsey@gmail.com>
2018-01-11 17:51:16 -0600

    Added 4th counter

--- public/controllers/inputController.js
+++ public/controllers/inputController.js
@@ -3,6 +3,7 @@ app.controller('inputControl', ['$scope', '$http', '$rootScope', function($scope
     $scope.quantity1 = 0;
 	$scope.quantity2 = 0;
 	$scope.quantity3 = 0;
+	$scope.quantity4 = 0;
 	var db = firebase.firestore();
 	var usersDB = db.collection('users');
 
