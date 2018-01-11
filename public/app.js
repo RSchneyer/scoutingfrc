fbb7a9d74553bfb54832c41bcac314a0fdb19a66
phekelsey <phekelsey@gmail.com>
2018-01-10 21:46:29 -0600

    Added Match input Directives

--- public/app.js
+++ public/app.js
@@ -1,37 +1,32 @@
-var app = angular.module('scoutingfrc', ['ngMaterial', 'firebase', 'ngSanitize', 'ngCsv', 'ngRoute']);
+var app = angular.module('scoutingfrc', ['ngMaterial', 'firebase', 'ngSanitize', 'ngCsv']);
 
-app.run(function($rootScope, $location){
+app.run(function($rootScope){
 	$rootScope.loggedIn = false;
-	$rootScope.newUser = false;
 	var db = firebase.firestore();
 	firebase.auth().onAuthStateChanged(function(user){
 		console.log('Auth State Changed');
 		if (user) {
-
 			var userDoc = db.collection('users').doc(user.uid);
 			//If document with user's uid exists in users collection, otherwise create uid named document and add displayname and email fields
 			if (userDoc.exists) {
 				//User already exists, log in as usual
 				console.log('User exists in Firestore');
 			} else {
-				//Set flag to display team register directive
-				$rootScope.$apply(function(){
-					$rootScope.newUser = true;
+				userDoc.set({
+					userDisplayName: user.displayName,
+					email: user.email
 				});
 			}
 			//Set $scope variables
 			$rootScope.$apply(function(){
 				$rootScope.user = user;
 				$rootScope.loggedIn = true;
-				$location.path('/dashboard'); //Direct user to scoutingfrc.com/dashboard if user is logged in
 			});
 		}
 		else {
 			console.log('error');
 		}
 	});
-
-	 
 
 	$rootScope.signIn = function(){
 		var provider = new firebase.auth.GoogleAuthProvider();
@@ -46,38 +41,11 @@ app.run(function($rootScope, $location){
 
 	$rootScope.signOut = function(){
 		firebase.auth().signOut().then(function(){
-			$rootScope.$apply(function(){
-				$rootScope.user = {};
-				$rootScope.loggedIn = false;
-				$location.path('/');
-			});
+			$rootScope.user = {};
+			$rootScope.loggedIn = false;
 		});
 	};
 });
-	
-// Angular Routing /////////////////////////////
-
-app.config(function($routeProvider, $locationProvider){
-	$routeProvider
-	.when('/', {
-		templateUrl: 'views/register.html',
-	})
-	.when('/dashboard', {
-		templateUrl: 'views/dashboard.html'
-	})
-	.when('/matches', {
-		templateUrl:'views/matches.html'
-	})
-	.when('/download', {
-		templateUrl:'views/download.html'
-	})
-	.when('/about', {
-		templateUrl: 'views/about.html'
-	});
-	$locationProvider.html5Mode(true);
-});
-
-
 
 
 //Directives ///////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -121,4 +89,113 @@ app.directive('signInCard', function(){
 	return {
 		templateUrl: 'directives/signInCard.html',
 	};
+});
+app.directive('preMatchCard', function(){
+	return {
+		templateUrl: 'directives/preMatchCard.html',
+	};
+});
+app.directive('autoCard', function(){
+	return {
+		templateUrl: 'directives/autoCard.html',
+	};
+});
+app.directive('endGame', function(){
+	return {
+		templateUrl: 'directives/endGame.html',
+	};
+});
+app.directive('teleop', function(){
+	return {
+		templateUrl: 'directives/teleop.html',
+	};
+});
+app.directive('counter', function() {
+    return {
+        restrict: 'A',
+        scope: { value: '=value' },
+        template: '<a href="javascript:;" class="counter-minus" ng-click="minus()">-</a>\
+                  <input type="text" class="counter-field" ng-model="value" ng-change="changed()" ng-readonly="readonly">\
+                  <a  href="javascript:;" class="counter-plus" ng-click="plus()">+</a>',
+        link: function( scope , element , attributes ) {
+            // Make sure the value attribute is not missing.
+            if ( angular.isUndefined(scope.value) ) {
+                throw "Missing the value attribute on the counter directive.";
+            }
+            
+            var min = angular.isUndefined(attributes.min) ? null : parseInt(attributes.min);
+            var max = angular.isUndefined(attributes.max) ? null : parseInt(attributes.max);
+            var step = angular.isUndefined(attributes.step) ? 1 : parseInt(attributes.step);
+            
+            element.addClass('counter-container');
+            
+            // If the 'editable' attribute is set, we will make the field editable.
+            scope.readonly = angular.isUndefined(attributes.editable) ? true : false;
+            
+            /**
+             * Sets the value as an integer.
+             */
+            var setValue = function( val ) {
+                scope.value = parseInt( val );
+            }
+            
+            // Set the value initially, as an integer.
+            setValue( scope.value );
+            
+            /**
+             * Decrement the value and make sure we stay within the limits, if defined.
+             */
+            scope.minus = function() {
+                if ( min && (scope.value <= min || scope.value - step <= min) || min === 0 && scope.value < 1 ) {
+                    setValue( min );
+                    return false;
+                }
+                setValue( scope.value - step );
+            };
+            
+            /**
+             * Increment the value and make sure we stay within the limits, if defined.
+             */
+            scope.plus = function() {
+                if ( max && (scope.value >= max || scope.value + step >= max) ) {
+                    setValue( max );
+                    return false;
+                }
+                setValue( scope.value + step );
+            };
+            
+            /**
+             * This is only triggered when the field is manually edited by the user.
+             * Where we can perform some validation and make sure that they enter the
+             * correct values from within the restrictions.
+             */
+            scope.changed = function() {
+                // If the user decides to delete the number, we will set it to 0.
+                if ( !scope.value ) setValue( 0 );
+                
+                // Check if what's typed is numeric or if it has any letters.
+                if ( /[0-9]/.test(scope.value) ) {
+                    setValue( scope.value );
+                }
+                else {
+                    setValue( scope.min );
+                }
+                
+                // If a minimum is set, let's make sure we're within the limit.
+                if ( min && (scope.value <= min || scope.value - step <= min) ) {
+                    setValue( min );
+                    return false;
+                }
+                
+                // If a maximum is set, let's make sure we're within the limit.
+                if ( max && (scope.value >= max || scope.value + step >= max) ) {
+                    setValue( max );
+                    return false;
+                }
+                
+                // Re-set the value as an integer.
+                setValue( scope.value );
+            };
+        }
+    }
 });
--- public/controllers/navController.js
+++ public/controllers/navController.js
@@ -1,27 +1,6 @@
-app.controller('navControl', ['$scope', '$mdSidenav', '$location',  function($scope, $mdSidenav, $location){
+app.controller('navControl', ['$scope', '$mdSidenav', function($scope, $mdSidenav){
 	$scope.togglesideNav = function(){
 		$mdSidenav('left').toggle();
 		console.log('SideNav toggled');
-	};
-
-	$scope.dashboard = function(){
-		console.log('dashboard');
-		$location.path('/dashboard');
-		$mdSidenav('left').toggle();
-	};
-	$scope.matches = function(){
-		console.log('matches');
-		$location.path('/matches');
-		$mdSidenav('left').toggle();
-	};
-	$scope.csv = function(){
-		console.log('csv');
-		$location.path('/download');
-		$mdSidenav('left').toggle();
-	};
-	$scope.about = function(){
-		console.log('about');
-		$location.path('/about');
-		$mdSidenav('left').toggle();
 	};	
 }]);
