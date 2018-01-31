a5a7ab04e6eb4ffe010297b34c56f2aa972ff3a9
Reid <reschneyer89@gmail.com>
2018-01-31 11:43:31 -0600

    Progress on Service Worker
    Trying to cache CDNs. Might use webpack to bundle them instead?

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,7 @@
 if ('serviceWorker' in navigator) {
 	navigator.serviceWorker.register('./service-worker.js').then(function(registration){
 		console.log(registration);
+		console.log(registration.scope);
 	}).catch(function(error){
 		console.log(error);
 	});
@@ -8,33 +9,38 @@ if ('serviceWorker' in navigator) {
 	console.log('Service Worker is not supported in this browser.');
 }
 
+const offlineFiles = [
+	'/',
+	'index.html',
+	'scouting.css',
+	'manifest.json',
+	'databasefavicon.png',
+	'/views/matches.html',
+	'/views/dashboard.html',
+	'/directives/autoCard.html',
+	'/directives/endGame.html',
+	'/directives/loadTeamDataCard.html',
+	'/directives/preMatchCard.html',
+	'/directives/scoutMatchCard.html',
+	'/directives/sideNav.html',
+	'/directives/teamInputCard.html',
+	'/directives/teamStatsCard.html',
+	'/directives/teleop.html',
+	'/controllers/inputController.js',
+	'/controllers/navController.js',
+	'/controllers/outputController.js',
+];
+
 
 self.addEventListener('install', function(event){
 	console.log(event);
 	event.waitUntil(
 		caches.open('v1').then(function(cache){
-			return cache.addAll([
-				'/',
-				'index.html',
-				'scouting.css',
-				'manifest.json',
-				'databasefavicon.png',
-				'/views/matches.html',
-				'/views/dashboard.html',
-				'/directives/autoCard.html',
-				'/directives/endGame.html',
-				'/directives/loadTeamDataCard.html',
-				'/directives/preMatchCard.html',
-				'/directives/scoutMatchCard.html',
-				'/directives/sideNav.html',
-				'/directives/teamInputCard.html',
-				'/directives/teamStatsCard.html',
-				'/directives/teleop.html',
-				'/controllers/inputController.js',
-				'/controllers/navController.js',
-				'/controllers/outputController.js',
-
-			]);	
+			cache.add("https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.6.5/angular.min.js");
+			cache.add("https://cdnjs.cloudflare.com/ajax/libs/angular-material/1.1.5/angular-material.min.js");
+			cache.add("https://cdnjs.cloudflare.com/ajax/libs/angular-material/1.1.5/angular-material.min.css");
+			cache.add("https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.6.5/angular-route.min.js");
+			return cache.addAll(offlineFiles);	
 		})
 	);
 });
@@ -45,8 +51,8 @@ self.addEventListener('activate', function(event){
 
 self.addEventListener('fetch', function(event){
 	console.log('FETCHING!!!!!');
-	console.log(event.request);
+	console.log(event.request.href);
 	event.respondWith(
-		caches.match('/views/'+event.request+'.html')
+		caches.match('index.html')
 	);
 });
